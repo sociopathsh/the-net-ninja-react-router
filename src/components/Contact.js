1578e2e44dcac181fcf5fdec5e2883ee55f395f9
SoeHtut <soehtut@escapepixel.com>
2019-01-15 12:32:21 +0630

    Programmatic Redirects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-const Contact = () => {
+const Contact = props => {
+  // setTimeout(() => {
+  //   props.history.push("/about");
+  // }, 2000);
   return (
     <div>
       <h1>Contact</h1>
