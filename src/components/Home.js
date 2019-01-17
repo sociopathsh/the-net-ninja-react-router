ff9eb534aa818e3254e5e6595742af20754533b1
SoeHtut <soehtut@escapepixel.com>
2019-01-17 14:15:19 +0630

    importing images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Pokeball from "../pokeball.png";
 
 class Home extends Component {
   state = {
@@ -19,9 +20,10 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="The poke ball" />
             <div className="card-content">
               <Link to={"/" + post.id}>
-                <span className="card-title">{post.title}</span>
+                <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
@@ -32,7 +34,7 @@ class Home extends Component {
       <div className="center">No posts yet!</div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h1>Home</h1>
         {postList}
       </div>
